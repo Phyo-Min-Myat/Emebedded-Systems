cdda911cce5143f08399f0116eca5f33363f2010
PhyoMinMyat <simonphyomin@gmail.com>
2023-02-24 20:14:28 +0630

    Creating Initial Coursera Repository

---
+++ statis.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "statis.h"
+
+int main(){
+
+    return 0;
+}
+
+void print_statistics(){
+    printf("This is the print function!");
+}
+
+void print_array(){
+    printf("This is the print_array function!");
+}
+
+void find_median(){
+    printf("This is the find_median function!");
+}
+
+void find_mean(){
+    printf("This is the find_mean function!");
+}
+
+void find_maximun(){
+    printf("This is the find_maximun function!");
+}
+
+void find_minimum(){
+    printf("This is the find_minimum function!");
+}
+
+void sort_array(){
+    printf("This is the sort_arry function!");
+}
---
+++ statis.h
@@ -0,0 +1,20 @@
+/**
+ * @brief  Reverse the byte order of an unsigned 32-bit integer
+ * 
+ * This function takes as an input a 32-bit integer. This will
+ * reverse the byte order of the full word. Insted of changing the 
+ * parameter passed it, it will returned a reeversed unsigned 32-bit 
+ * integer.
+ * 
+ * @param word The 32-bit word to be reversed
+ * @return The reversed 32-bit value word
+ * 
+ */
+unsigned int swap32(unsigned int word);
+void print_statistics();
+void print_array();
+void find_median();
+void find_mean();
+void find_maximun();
+void find_minimum();
+void sort_array();
